54cad55bb50b80dd7739af478adb7150a3f21ac4
thanhtungf123 <thanhtungphankt@gmail.com>
2025-10-17 13:49:36 +0700

    chore: update .gitignore for Java/Maven and allow source push

--- src/main/java/com/luxestay/hotel/dto/auth/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.luxestay.hotel.dto.auth;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- src/test/java/HotelApplicationTests.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HotelApplicationTests {
-    @Test
-    void contextLoads() {}
-}
